09a3ce5a9db50d99ba569c0636d9a8e23a1d7a07
A H M Golam Hafiz <hasnathafiz229@gmail.com>
2024-09-30 14:51:00 -0500

    removed no argment constructor

--- src/main/java/com/example/lab01/model/Book.java
+++ src/main/java/com/example/lab01/model/Book.java
@@ -2,11 +2,9 @@ package com.example.lab01.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
-@NoArgsConstructor
 public class Book {
     private int id;
     private String title;
